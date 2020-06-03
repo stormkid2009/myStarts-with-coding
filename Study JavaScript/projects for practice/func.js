9ed0c25dd4698b1a50ec96dd1e123378468306f8
Sherif Ahmed <stormyshadow2009@outlook.com>
2020-06-03 21:42:12 +0200

    chapter 3 Eloquent javascript is done

--- Study JavaScript/projects for practice/func.js
+++ Study JavaScript/projects for practice/func.js
@@ -14,6 +14,13 @@ if num=0 then it is even=true
 if num=1 then it is odd=false
 if num=negative value then -- will make it positive  and the recursion that
 every time the function call itself  will reduce num by 2 until the value gonna be 1 or 0 */
+//my update for the last function
+function check(num){
+    if(num<0){return check(-num)}
+    else if(num%2===0){return `${num} is even !`}
+    else{return `${num} is odd !`}
+}
+console.log(check(31));
 
 function countChar(string,char) {
     let count = 0;
