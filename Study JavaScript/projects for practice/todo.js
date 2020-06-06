c484b2d1f207ac8054bbdf86dc9b1ed77a23fbf3
Sherif Ahmed <stormyshadow2009@outlook.com>
2020-06-07 01:26:41 +0200

    ok we update our tasks array

--- Study JavaScript/projects for practice/todo.js
+++ Study JavaScript/projects for practice/todo.js
@@ -1,20 +1,29 @@
+let toDoList = ["walk the dog", "go to the gym", "watch a movie"];
+//we will update our list throw this function
+function updateArr(item) {
+    toDoList.push(item);
 
-      const userInput = document.querySelector("#task");
-      userInput.addEventListener("change", (e) => {
-        let addNewTask = e.target.value;
-        toDoList.unshift(addNewTask);
-        console.log(addNewTask);
-      });
-      let toDoList = ["walk the dog", "go to the gym", "watch a movie"];
-      let list = document.getElementById("list");
-      for (let item = 0; item < 4; item++) {
-        //creating unorder list li
-        let el = document.createElement("li");
-        //appending the new element to the dom
-        list.appendChild(el);
-        //give the item unique id the same we generate with the for loop
-        el.setAttribute("id", item);
-        //change value of li inner html with the array item
-        document.getElementById(item).textContent = toDoList[item];
-      }
-    
+}
+//when user input a task it will be added to our toDoList array
+const userTask = document.getElementById("userInput");
+userTask.addEventListener("change", (e) => {
+    let addNewTask = e.target.value;
+    updateArr(addNewTask);
+
+});
+console.log(toDoList);
+
+
+
+
+    //   let list = document.getElementById("list");
+    //   for (let item = 0; item < 4; item++) {
+    //     //creating unorder list li
+    //     let el = document.createElement("li");
+    //     //appending the new element to the dom
+    //     list.appendChild(el);
+    //     //give the item unique id the same we generate with the for loop
+    //     el.setAttribute("id", item);
+    //     //change value of li inner html with the array item
+    //     document.getElementById(item).textContent = toDoList[item];
+    //   }
