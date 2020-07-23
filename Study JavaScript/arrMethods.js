1c028225e6ac9cab1a5bde7e86a37181b7f63c4f
Sherif Ahmed <stormyshadow2009@outlook.com>
2020-07-23 09:53:03 +0200

    just checking array methods

--- Study JavaScript/arrMethods.js
+++ Study JavaScript/arrMethods.js
@@ -11,15 +11,17 @@ toDo.indexOf('watch a movie');
 //it just free space from the memory
 //delete toDo[0];
 toDo.splice(0,1);
-//splice syntax =>  arr.splice(index,number of items to delete)
+//splice syntax =>  arr.splice(index,number of items to delete,
+//[optional]items to add)
+//notice that splice will make changes to the elements of the array 
 //console.log(toDo);
 
 //console.log(toDo.indexOf('watch a movie'));
 let list=['banana', 'orange', 'apple', 'grapes', 'berry','coconut'];
 let newArr=list.slice(1,3);
 /*slice does not change the array but return a new array with certain elements
-slice syntax => arr.slice(index to start "it is inclusive in the new array",index to end "exclusive 
-in the new array")*/
+slice syntax => arr.slice(index to start "it is inclusive in the new array"
+,index to end "exclusive in the new array")*/
 //console.log(newArr);
 //let myArr=list.concat(newArr);
 //concat does not change original arrays but it merges them in new array
